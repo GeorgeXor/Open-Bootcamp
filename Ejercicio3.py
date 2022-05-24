9f8542eece81c01ffaf40b8abdbee43c76620442
GeorgeXor <42229945+GeorgeXor@users.noreply.github.com>
2022-05-24 16:54:37 -0500

    Add files via upload
    Ejercicio 3

---
+++ Ejercicio3.py
@@ -0,0 +1,8 @@
+'''
+En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, 
+tendrás que imprimir también el tipo de la variable por consola.
+'''
+
+cadena = 'Este es el ejercicio 3 del Bootcamp'
+print(cadena)
+print(f'La variable es del tipo {type(cadena)}')
